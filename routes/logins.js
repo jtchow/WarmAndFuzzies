8870c37c96de7a7cf8b6756ffe1006c80f827c52
jtchow <chowjt@uci.edu>
2020-04-04 18:15:27 -0700

    defined base user model

--- routes/logins.js
+++ routes/logins.js
@@ -4,6 +4,21 @@ const router = express.Router()
 
 router.post('/signup', function(req,res) {
     res.send('signing up')
+    const name = req.body.name;
+    const email = req.body.email;
+    const password = req.body.password;         // TODO encrypt password somewhere/ don't use plaintext
+    // TODO add metadata like date 
+
+    const newUser = new Exercise({
+        username,
+        description,
+        duration,
+        date,
+    });
+
+    newExercise.save()
+    .then(() => res.json('Exercise added!'))
+    .catch(err => res.status(400).json('Error: ' + err))
 });
 
 router.post('/login', function(req,res) {
