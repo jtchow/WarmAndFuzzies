a9c5c569bc39b65947b73a588afef5ca23b78374
jtchow <chowjt@uci.edu>
2020-04-04 21:15:17 -0700

    created note model

--- models/user.model.js
+++ models/user.model.js
@@ -2,44 +2,47 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
-const userSchema = new Schema({
-    username: {
-        type: String,
-        required: true,
-        trim: true,
-        minlength: 3
-    },
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            trim: true,
+            minlength: 3
+        },
 
-    firstName: {
-        type: String,
-        required: true,
-        trim: true,
-    },
+        firstName: {
+            type: String,
+            required: true,
+            trim: true,
+        },
 
-    lastName: {
-        type: String,
-        required: true,
-        trim:true
-    },
+        lastName: {
+            type: String,
+            required: true,
+            trim:true
+        },
 
-    email: {
-        type: String,
-        required: true,
-        trim:true, 
-        minlength: 5
-    },
+        email: {
+            type: String,
+            required: true,
+            trim:true, 
+            minlength: 5
+        },
 
-    // TODO password hash
-    password: {                 
-        type: String,
-        required: true,
-        trim:true, 
-        minlength: 7
+        // TODO password hash
+        password: {                 
+            type: String,
+            required: true,
+            trim:true, 
+            minlength: 7
+        }
+    }, 
+    
+    {
+        timestamps: true
     }
-
-}, {
-    timestamps: true,
-});
+);
 
 const User = mongoose.model('User', userSchema);
 
