b1de2b6cd3d67fe917c9123f081f40e2c74cefc0
jtchow <chowjt@uci.edu>
2020-04-04 22:38:52 -0700

    removed exercise route files

--- server.js
+++ server.js
@@ -12,14 +12,14 @@ app.use(express.json());
 const port = process.env.PORT || 5000;
 
 
-// // create mongoDB connection
-// const uri = process.env.ATLAS_URI;
-// mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true }
-// );
-// const connection = mongoose.connection;
-// connection.once('open', () => {
-//     console.log("MongoDB database connection established successfully");
-// });
+// create mongoDB connection
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true }
+);
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log("MongoDB database connection established successfully");
+});
 
 
 
