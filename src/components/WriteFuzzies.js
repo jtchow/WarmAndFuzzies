09ccfa9fa2166372529d6a778affdfdeaf0a690b
anniehirata <anniehirata2@gmail.com>
2020-04-06 22:36:12 -0700

    Changed layout of select recipient input and filter

--- src/components/WriteFuzzies.js
+++ src/components/WriteFuzzies.js
@@ -14,7 +14,8 @@ export default class WriteFuzzies extends React.Component
             sender: props.userid,
             recipient: '',
             message: '',
-            filter: 'all'
+            filter: 'all',
+            filterText: 'Filter'
         };
 
         this.handleRecipientChange = this.handleRecipientChange.bind(this);
@@ -40,7 +41,8 @@ export default class WriteFuzzies extends React.Component
     handleFilterChange(event)
     {
         this.setState({
-            filter: event.target.value
+            filter: event.target.value,
+            filterText: "All"
         });
     }
 
@@ -91,7 +93,7 @@ export default class WriteFuzzies extends React.Component
             {name: "Richard", id: 10},
             {name: "Sarah", id: 11},
             {name: "Tyler Onishi", id: 12},
-            {name: "Tyler Yin", id: 13}
+            {name: "Tyler Yin", id: 13},
         ];
 
         const writtenTo = [1,3,4,5]; //list of ids of users who the user has written to--also get this from api
@@ -102,7 +104,7 @@ export default class WriteFuzzies extends React.Component
         const filterNotWritten = (recipient) => (!writtenTo.includes(recipient.id));
 
         // filter options here
-        const filterFunc = this.state.filter === "any" ? noFilter : this.state.filter === "written" ? filterWritten : filterNotWritten;
+        const filterFunc = this.state.filter === "all" ? noFilter : this.state.filter === "written" ? filterWritten : filterNotWritten;
         const options = recipients.filter(filterFunc).map((recipient) => (<option key={recipient.id} value={recipient.id}>{recipient.name}</option>));
         //name list needs to be passed from props or use api call somewhere
 
@@ -114,20 +116,16 @@ export default class WriteFuzzies extends React.Component
                         <h3 id="write-label">Select a recipient and write a warm and fuzzy!</h3>
                         <br/>
 
-                        <div id="filter-label">
-                            <p>Filter</p>
-                        </div>
-
-                        <div className="fuzzies-forms">
-                            <select id="filter-select" className="form-control" onChange={this.handleFilterChange}>
-                                <option value="all">All</option>
-                                <option value="not-written">Not written to</option>
-                                <option value="written">Written to</option>
+                        <div className="fuzzies-forms" id="select-div">
+                            <select id="recipient-select" className="form-control" value={this.state.recipient} onChange={this.handleRecipientChange}>
+                                {this.state.recipient === "" ? <option value="">Select a recipient</option> : ""}
+                                {options}
                             </select>
 
-                            <select className="form-control" value={this.state.recipient} onChange={this.handleRecipientChange}>
-                                <option value="">Select a recipient</option>
-                                {options}
+                            <select id="filter-select" className="form-control" onChange={this.handleFilterChange}>
+                                <option value="all">{this.state.filterText}</option>
+                                <option value="not-written">Not written to</option>
+                                <option value="written">Written to</option>
                             </select>
                         </div>
                     </div>
