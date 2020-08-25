b218d610ddd74e09740ba681692c099f31831d39
anniehirata <anniehirata2@gmail.com>
2020-08-25 16:41:36 -0700

    Added upload file input to edit user page

--- src/components/EditUser.js
+++ src/components/EditUser.js
@@ -11,7 +11,8 @@ import {withRouter} from 'react-router';
         this.state = {
             username: this.props.cookies.get('user'),
             first: '',
-            last: ''
+            last: '',
+            picture: ''
         }
 
         this.handleSubmit = this.handleSubmit.bind(this);
@@ -33,6 +34,7 @@ import {withRouter} from 'react-router';
     
     handleSubmit(event)
     {
+        console.log(this.state);
         event.preventDefault();
         
         axios.post("http://localhost:5000/user/update", null, {
@@ -65,6 +67,8 @@ import {withRouter} from 'react-router';
                     <input className="form-control edit-input" type = "text" value = {this.state.first} onChange={(e) => this.setState({first:e.target.value})}></input>
                     <label className="edit-label">Last Name</label>
                     <input className="form-control edit-input" type = "text" value = {this.state.last} onChange={(e) => this.setState({last: e.target.value})}></input>
+                    <label className="edit-label">Profile Picture</label>
+                    <input className="form-control-file edit-input" type="file" value = {this.state.picture} onChange={(e) => this.setState({picture: e.target.value})}></input>
                     </div>
                     <button type="submit" className="btn btn-primary mb-2" id="profile-save-button">Save</button>
                     <button type="button" className="btn btn-primary mb-2" id="profile-cancel-button" onClick={this.handleCancel}>Cancel</button>
